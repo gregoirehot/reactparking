eef09490295a0d5d845c0f97cd5a8f28bfbac343
Grégoire Hot <ghot@PROXIAD.NORD>
2018-05-31 15:45:30 +0200

    with react-router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import TableParking from './containers/TableParking';
 import ScoreBoard from './containers/ScoreBoard';
 import './App.css';
@@ -7,8 +8,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <TableParking />
-        <ScoreBoard />
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Parking App</Link>
+              </li>
+              <li>
+                <Link to="/ScoreBoard">ScoreBoard App</Link>
+              </li>
+            </ul>
+
+            <hr />
+
+            <Route exact path="/" component={TableParking} />
+            <Route path="/ScoreBoard" component={ScoreBoard} />
+          </div>
+        </Router>
       </div>
     );
   }
